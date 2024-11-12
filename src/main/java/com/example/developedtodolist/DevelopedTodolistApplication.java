9d0b0e67493c98856e5b256886cf0ca372e6a195
2unmi <2unmin4030@gmail.com>
2024-11-12 23:26:16 +0900

    init commit

---
+++ src/main/java/com/example/developedtodolist/DevelopedTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.developedtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopedTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopedTodolistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/developedtodolist/DevelopedTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developedtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopedTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
